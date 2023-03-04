d196a915993676fee03cdbf06d8d9a7b89b9b205
Ivo Yordanov <ivosvetcavicata@gmail.com>
2023-03-04 14:20:32 +0100

    Javascript Clase1

---
+++ Javascript/Clase1/js/code.js
@@ -0,0 +1,18 @@
+var x = 4;
+
+
+function sum(a, b) {
+    let x = 5;
+    console.log(x);  
+    return a + b; 
+}
+
+function sub(a, b) {
+    console.log(x);
+    return a - b;
+}
+
+sum(1,2);
+console.log(x);
+sub(1, 2);
+console.log(x)
---
+++ Javascript/Clase1/js/main.js
@@ -0,0 +1,24 @@
+function sum(a, b) {
+    console.log("He Entrado");
+    window.pepito = "grillo";
+    return a + b;
+}
+sum(3, 4);
+console.log(pepito);
+x = sum(3,4);
+x = sum(x, x);
+console.log(x);
+
+let a = 4;
+let b = 50;
+a++
+let productos = {
+    nombre: "Patata",
+    peso: 5,
+    precio: 10
+}
+console.log(productos.nombre);
+productos.peso = 6;
+console.log(productos.peso);
+productos.comercio = "Carrefour"
+console.log(productos);
